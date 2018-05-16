3ec58102882733b05c9360cdf419a657f91ac623
vikrep <protsenko.viktor@gmail.com>
2018-05-16 11:36:27 +0100

    post req

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const apiRouter = require("./api");
 const app = express();
 const router = express.Router();
 
+const dbFilename = 'mydatabase.sqlite';
+const sqlite = require('sqlite3').verbose();
+let db = new sqlite.Database(dbFilename);
+
+
 app.engine(
   "hbs",
   exphbs({
@@ -26,10 +31,61 @@ app.use("/api", apiRouter);
 // handle HTTP POST requests
 app.use(bodyparser.json());
 
-app.get("/", function(req, res, next) {
+app.get("/", function (req, res, next) {
   res.render("home");
 });
 
 app.listen(SERVER_PORT, () => {
   console.info(`Server started at http://localhost:${SERVER_PORT}`);
+});
+
+app.get("/customers", function (req, res) {
+  db.all("SELECT title, firstname, surname " +
+    "FROM customers", function (err, rows) {
+      rows.forEach(function (row) {
+        console.log(row.title, row.firstname,
+          row.surname);
+      });
+      res.status(200).json({
+        customers: rows
+      });
+    });
+});
+
+app.get("/customers/:id", function (req, res) {
+  var id = req.params.id;
+  db.get("SELECT * FROM customers WHERE id = ?", [id],
+    function (err, row) {
+      res.status(200).json({
+        customer: row
+      });
+    });
+});
+
+app.get("/customers/name/:surname", function (req, res) {
+  var surname = req.params.surname;
+  db.get("SELECT * FROM customers WHERE surname = ?", [surname],
+    function (err, row) {
+      res.status(200).json({
+        customer: row
+      });
+    });
+});
+
+app.post("/customers/", function (req, res) {
+  var ttl = req.body.title;
+  var fnm = req.body.firstname;
+  var snm = req.body.surname;
+  var addr = req.body.addr;
+  console.log(req.body.firstname);
+  db.run("INSERT INTO customers (title, firstname, surname, address) VALUES (?, ?, ?, ?)",
+      [ttl, fnm, snm, addr], function(err) {
+      if (err == null) {
+        var rowid = this.lastID;  //get the PK
+        console.log("New customer id = ${rowid}");
+        res.status(200).json({lastId: rowid.toString()});  // return the PK
+      } else {
+        res.status(500).json({error: err});
+      }
+  });
 });
